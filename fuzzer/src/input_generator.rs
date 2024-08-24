bf68876a0ec150dac9d15d2205f00a7598082907
UVvirus <yuvasyuvarajan@gmail.com>
2024-08-24 16:32:42 +0530

    testing word function

--- fuzzer/src/input_generator.rs
+++ fuzzer/src/input_generator.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+
 pub fn generate_random_number(min: u32, max: u32)-> u32{
     let mut rng = rand::thread_rng();
     let random_number = rng.gen_range(min..=max);
@@ -8,7 +9,10 @@ pub fn generate_random_number(min: u32, max: u32)-> u32{
 pub fn generate_random_word(word_length: u32) -> String{
     let letters='a'..='z';
     let mut rng = rand::thread_rng();
-    let random_word: String = letters;
+    let random_word = (0..word_length).map(|_| {
+        let char = rng.gen_range(letters.clone());
+        char
+    }).collect();
     println!("Random word: {}", random_word);
     random_word
 }
--- fuzzer/src/main.rs
+++ fuzzer/src/main.rs
@@ -1,14 +1,15 @@
 use std::process::Command;
 use std::io::Error;
-use input_generator::generate_random_number;
-use input_generator::generate_random_word;
+// use input_generator::generate_random_number;
+// use input_generator::generate_random_word;
+mod input_generator;
 
 fn main() -> Result<(), Error>{
-let random_number=generate_random_number(1,100);
+let random_number=input_generator:: generate_random_number(1,100);
 println!("Random number: {}", random_number);
 
-let random_word = generate_random_word(10);
-
+let random_word = input_generator::generate_random_word(10);
+println!("Random word: {}", random_word);
 let python_script = "/home/you_we/test.py";
 println!("Running command: python3 {}", python_script);
 let output = Command::new("python").arg(python_script)
